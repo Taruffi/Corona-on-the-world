e29029f545add99e8754b0ac21d04ef4aae4e178
Taruffi <taruffi.igor@gmail.com>
2020-04-07 18:17:21 -0300

    Updates

---
+++ precache-manifest.001f7bdfa25ea7978eaac698eca930e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a68d24a474c9ae83c5c62ebd76677d",
+    "url": "/Corona-on-the-world/index.html"
+  },
+  {
+    "revision": "999948032c8e87e5bc67",
+    "url": "/Corona-on-the-world/static/css/main.73771095.chunk.css"
+  },
+  {
+    "revision": "b5f6a454dd6b8d7dcf1b",
+    "url": "/Corona-on-the-world/static/js/2.c607cc14.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-on-the-world/static/js/2.c607cc14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999948032c8e87e5bc67",
+    "url": "/Corona-on-the-world/static/js/main.b8f9bd4d.chunk.js"
+  },
+  {
+    "revision": "ba1e985714a8a947a47f",
+    "url": "/Corona-on-the-world/static/js/runtime-main.31d6490d.js"
+  },
+  {
+    "revision": "8e9ec67e63be092461d05a62af5edf56",
+    "url": "/Corona-on-the-world/static/media/image2.8e9ec67e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Taruffi/Corona-on-the-world/precache-manifest.a58273646929e69f55aa5d8c48cf5329.js"
+  "/Corona-on-the-world/precache-manifest.001f7bdfa25ea7978eaac698eca930e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Taruffi/Corona-on-the-world/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Corona-on-the-world/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
