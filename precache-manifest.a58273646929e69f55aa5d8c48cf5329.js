67fd0ea22ace495d0437997dd556a9e3bc6bbf2a
Taruffi <taruffi.igor@gmail.com>
2020-04-07 17:54:30 -0300

    Updates

---
+++ precache-manifest.a58273646929e69f55aa5d8c48cf5329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94bad694fb2b6cde54d43d7f5ea3f44",
+    "url": "/Taruffi/Corona-on-the-world/index.html"
+  },
+  {
+    "revision": "999948032c8e87e5bc67",
+    "url": "/Taruffi/Corona-on-the-world/static/css/main.73771095.chunk.css"
+  },
+  {
+    "revision": "917c9f3272d2d78817a8",
+    "url": "/Taruffi/Corona-on-the-world/static/js/2.8c1a23c5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Taruffi/Corona-on-the-world/static/js/2.8c1a23c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999948032c8e87e5bc67",
+    "url": "/Taruffi/Corona-on-the-world/static/js/main.b8f9bd4d.chunk.js"
+  },
+  {
+    "revision": "8459952abc772979e376",
+    "url": "/Taruffi/Corona-on-the-world/static/js/runtime-main.4e9dbf35.js"
+  },
+  {
+    "revision": "8e9ec67e63be092461d05a62af5edf56",
+    "url": "/Taruffi/Corona-on-the-world/static/media/image2.8e9ec67e.png"
+  }
+]);
